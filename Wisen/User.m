81bfac4d161175eaccec0922dd44c02dfbb45e04
Ryan Li <liyihe96@gmail.com>
2015-02-28 22:59:21 -0800

    changed API usage

--- Wisen/User.m
+++ Wisen/User.m
@@ -143,8 +143,12 @@
 
 - (void)observeSingleRequest:(Request *)request withBlock:(void (^)(Request *request))block {
     Firebase *requestRef = [[Firebase alloc] initWithUrl:[NSString stringWithFormat:@"https://wisen.firebaseio.com/requests/%@", request.requestID]];
-    [requestRef observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-        if (block && snapshot.value != [NSNull null]) {
+    __block BOOL firstEvent = YES;
+    FirebaseHandle handle = [requestRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        if (firstEvent) {
+            firstEvent = NO;
+        } else if (block && snapshot.value != [NSNull null]) {
+            [requestRef removeAuthEventObserverWithHandle:handle];
             block([[Request alloc] initWithDictionary:snapshot.value]);
         }
     }];
